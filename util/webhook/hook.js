d6691fdba8d5ff435b02451eb1c3c3457e027115
benjaminran <benjaminran2@gmail.com>
2015-09-06 13:17:36 -0700

    adding gith webhook

---
+++ util/webhook/hook.js
@@ -0,0 +1,23 @@
+/* Webhook Listener
+*
+*  Requires that NodeJS package Gith be installed (with npm use `npm install gith`)
+*/
+
+
+// Listen on port 9001
+var gith = require('gith').create( 9001 );
+// Import execFile, to run our bash script
+var execFile = require('child_process').execFile;
+
+gith({
+    repo: 'fideloper/example'
+}).on( 'all', function( payload ) {
+    if( payload.branch === 'staging' )
+    {
+        // Exec a shell script
+        execFile('/path/to/hook.sh', function(error, stdout, stderr) {
+            // Log success in some manner
+            console.log( 'exec complete' );
+        });
+    }
+});
